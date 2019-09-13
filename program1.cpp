47552731588a40f68743bde92d11d12e4574d195
Chandler Haukap <chaukap@uwyo.edu>
2019-09-12 20:31:00 -0600

    continued work

--- program1.cpp
+++ program1.cpp
@@ -1,13 +1,44 @@
 #include<FlexLexer.h>
 
+/* This is a small program so I just made these globals */
+int row;
+int column;
+int rtn;
+yyFlexLexer myScanner;
+
 int main()
 {
-	yyFlexLexer myScanner;
-	int rtn;
-	printf("running\n");
+	/* ---- Initialization ---- */
+	row = 1;
+	column = 1;
+
+	/* ----- Print Header ----- */
+	printf("Line\tColumn\tType\tLength\tValue\n");
 	
-	while((rtn = myScanner.yylex()) != 0){
-		printf("returned %d\n", rtn);
+	/* ----- Loop through the input file ----- */
+	while((rtn = myScanner.yylex()) != 0) {
+		/* ------- Newline -------- */
+		if(rtn == 11){
+			printf("%d\t%d\t%d\t%d\n", row - 1, column, rtn, myScanner.YYLeng());
+			column = 1;
+
+		/* ---- A single space ---- */
+		} else if(rtn == 12) {
+			column++;
+
+		/* ----- A single tab ----- */
+		} else if(rtn == 13) {
+			column += 8 - (column % 8);
+
+		} else if(rtn == 10){
+			printf("%d\t%d\t%d\t%d\t%s\n", row, column, rtn, myScanner.YYLeng(), myScanner.YYText());
+			column += myScanner.YYLeng();
+			
+		/* ----- All other recognized symbols ----- */
+		} else {
+			printf("%d\t%d\t%d\t%d\t%s\n", row, column, rtn, myScanner.YYLeng(), myScanner.YYText());
+			column += myScanner.YYLeng();
+		}
 	}
 
 }
